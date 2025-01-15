91763b9508ad70a4035e6ca4b47369210a613d68
sakibsarker <sakib15-3824@diu.edu.bd>
2025-01-15 14:48:03 +0600

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,4 @@ export default defineConfig({
   build: {
     outDir: "build", // This ensures Vite puts the build files in the 'build' folder
   },
-  base: "https://moddhopayfrontend-q6ha.vercel.app/",
 });
